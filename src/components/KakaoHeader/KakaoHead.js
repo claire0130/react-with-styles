9a3824e9711860853b4288c167c152c5332439df
claire0130 <claire10040130@gmail.com>
2019-05-23 22:53:49 +0900

    Change. inline-style + CSS moudle 방식

--- src/components/KakaoHeader/KakaoHead.js
+++ src/components/KakaoHeader/KakaoHead.js
@@ -1,30 +1,36 @@
-// classnames 라이브러리 사용
+// 방법3. inline-style + CSS moudle
 import React from 'react';
 import logoBlack from 'static/images/logo_black.png';
-import classNames from 'classnames/bind';
-import styles from './KakaoHead.sass';
-const cx = classNames.bind(styles);
+import {
+  img_comm,
+  screen_out,
+  hide,
+  head_event,
+  list_util,
+  link_home,
+  link_today } from './KakaoHead.module.sass';
 
 const KakaoHead = () => {
   return (
-    <>  
-      <div id="kakaoHead" className={cx('head_event')}>
-      <h1 className={cx('tit_logo')}>
-        <a href="#none" id="kakaoServiceLogo" className={cx('link_logo')}>
-          <img src={logoBlack} width="183" height="53" className={cx('img_thumb')} alt="Black Desert Online REMASTERED" />
+    <>
+    <div id="kakaoHead" className={head_event}>
+      <h1 style={{position:"absolute",top:"23px",left:"50px"}}>
+        <a href="#none" id="kakaoServiceLogo" style={{display:"block"}}>
+          <img src={logoBlack} width="183" height="53" style={{verticalAlign:"top"}} alt="Black Desert Online REMASTERED" />
         </a>
       </h1>
 
-        <strong className={cx('screen_out')}>서비스 바로가기</strong>
-        <ul className={cx('list_util')}>
-          <li>
-            <a href="#none" className={cx('img_comm', 'link_home')}>검은사막 홈페이지 바로가기</a>
-          </li>
-          <li>
-            <a href="#none" className={cx('img_comm', 'link_today')}>이벤트 페이지 오늘하루 그만보기</a>
-          </li>
-        </ul>
-      </div>      
+      <strong className={screen_out}>서비스 바로가기</strong>
+      <ul className={list_util}>
+        <li>
+          <a href="#none" className={[img_comm, link_home].join(' ')}>검은사막 홈페이지 바로가기</a>
+        </li>
+        <li>
+          <a href="#none" className={[img_comm, link_today].join(' ')}>이벤트 페이지 오늘하루 그만보기</a>
+        </li>
+      </ul>
+    </div>
+    <hr className={hide} />
     </>
   );
 };
